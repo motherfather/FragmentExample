21e18c7e5820b9c25d71539eba585557cd61d7c3
origin <father@mail.com>
2016-11-30 11:02:52 +0900

    완료

--- app/src/main/java/com/bit2016/fragmentexample/MyFragment.java
+++ app/src/main/java/com/bit2016/fragmentexample/MyFragment.java
@@ -1,7 +1,13 @@
 package com.bit2016.fragmentexample;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -12,6 +18,23 @@ public class MyFragment extends Fragment {
     public MyFragment() {
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        TextView tv = new TextView(getActivity());
+//        tv.setText("I am Fragment");
+//        tv.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
 
+        View rootView = inflater.inflate(R.layout.layout_myfragment, container, false);
 
+        final View finalRootView = rootView;
+        rootView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)finalRootView.findViewById(R.id.textView)).setText("clicked");
+                getActivity().getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new MyFragment2()).commit();
+            }
+        });
+        return rootView;
+    }
 }
--- app/src/main/java/com/bit2016/fragmentexample/MyFragment2.java
+++ app/src/main/java/com/bit2016/fragmentexample/MyFragment2.java
@@ -1,10 +1,20 @@
 package com.bit2016.fragmentexample;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyFragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_myfragment2, container, false);
+    }
 }
